c7d8fcd1d8d636513b8b960b46be4a6c876b0929
hernah <hgalosada@gmail.com>
2024-11-10 03:55:34 +0800

    Align components to the center

--- src/components/SignalInput.vue
+++ src/components/SignalInput.vue
@@ -1,78 +1,91 @@
 <template>
-    <h4>Digital Signal Visualizer</h4>
-    <form @submit.prevent="encodeSignal" class="row g-2 align-items-center">
-      <div class="col-auto">
-        <label class="col-form-label" for="binaryInput">Binary Input</label>
+  <h4 class="text-center">Digital Signal Visualizer</h4>
+  <form @submit.prevent="encodeSignal" class="row g-2 align-items-center justify-content-center">
+    <div class="col-auto">
+      <label class="col-form-label" for="binaryInput">Binary Input</label>
+    </div>
+    <div class="col-4 col-md-4">
+      <input
+        id="binaryInput"
+        class="form-control"
+        type="text"
+        v-model="binaryData"
+        placeholder="Enter binary data (e.g., 1010)"
+      />
+    </div>
+    <div class="col-auto">
+      <button
+        class="btn btn-primary"
+        type="submit"
+        :disabled="isInputNotBinary"
+      >
+        Generate Digital Signal
+      </button>
+    </div>
+  </form>
+
+  <div v-if="encodedData" class="text-center mt-3">
+      <div class="my-2">
+        <NRZL :binaryData="encodedData"/>
       </div>
-      <div class="col-4 col-md-4">
-        <input
-          id="binaryInput"
-          class="form-control"
-          type="text"
-          v-model="binaryData"
-          placeholder="Enter binary data (e.g., 1010)"
-        />
+      <div class="my-2">
+        <NRZI :binaryData="encodedData"/>
       </div>
-      <div class="col-auto">
-        <button
-          class="btn btn-primary"
-          type="submit"
-          :disabled="isInputNotBinary"
-        >
-          Generate Digital Signal
-        </button>
+      <div class="my-2">
+        <BipolarAMI :binaryData="encodedData"/>
       </div>
-    </form>
-  
-    <NRZL v-if="encodedData" :binaryData="encodedData"/>
-    <NRZI v-if="encodedData" :binaryData="encodedData"/>
-    <BipolarAMI v-if="encodedData" :binaryData="encodedData"/>
-    <PseudoTernary v-if="encodedData" :binaryData="encodedData"/>
-    <ManchesterEncoding v-if="encodedData" :binaryData="encodedData" />
-    <DifferentialManchester v-if="encodedData" :binaryData="encodedData" />
-  </template>
-  
-  <script>
-  import NRZL from './NRZL.vue';
-  import NRZI from './NRZI.vue';
-  import BipolarAMI from './BipolarAMI.vue';
-  import PseudoTernary from './PseudoTernary.vue';
-  import ManchesterEncoding from './ManchesterEncoding.vue';
-  import DifferentialManchester from './DifferentialManchester.vue';
-  
-  export default {    
-    components: {
-      NRZL,
-      NRZI,
-      BipolarAMI,
-      PseudoTernary,
-      ManchesterEncoding,
-      DifferentialManchester
-    },
-    computed: {
-      isInputNotBinary() {
-        return !/^[01]+$/.test(this.binaryData);
-      }
-    },
-    data() {
-      return {
-        binaryData: "",
-        encodedData: ""
-      };
-    },
-    methods: {
-      encodeSignal() {
-        if (!this.isInputNotBinary) {
-          this.encodedData = this.binaryData;
-        }
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .invalid-feedback {
-    display: block;
+      <div class="my-2">
+        <PseudoTernary :binaryData="encodedData"/>
+      </div>
+      <div class="my-2">
+        <ManchesterEncoding :binaryData="encodedData"/>
+      </div>
+      <div class="my-2">
+        <DifferentialManchester :binaryData="encodedData"/>
+      </div>
+    </div>
+</template>
+
+<script>
+import NRZL from './NRZL.vue';
+import NRZI from './NRZI.vue';
+import BipolarAMI from './BipolarAMI.vue';
+import PseudoTernary from './PseudoTernary.vue';
+import ManchesterEncoding from './ManchesterEncoding.vue';
+import DifferentialManchester from './DifferentialManchester.vue';
+
+export default {    
+components: {
+  NRZL,
+  NRZI,
+  BipolarAMI,
+  PseudoTernary,
+  ManchesterEncoding,
+  DifferentialManchester
+},
+computed: {
+  isInputNotBinary() {
+    return !/^[01]+$/.test(this.binaryData);
   }
-  </style>
-  
+},
+data() {
+  return {
+    binaryData: "",
+    encodedData: ""
+  };
+},
+methods: {
+  encodeSignal() {
+    if (!this.isInputNotBinary) {
+      this.encodedData = this.binaryData;
+    }
+  },
+},
+};
+</script>
+
+<style scoped>
+.invalid-feedback {
+display: block;
+}
+</style>
